7ccf10104dbaccd7f9eb1b911d55011e4cc3a250
Sabarish575 <sabarish2k3@gmail.com>
2026-03-05 10:18:23 +0530

    test-3

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,8 @@ export function extractAndStoreToken(){
     const params=new URLSearchParams(window.location.search);
     const token=params.get("token");
 
+    console.log("this is your token ",token);
+    
 
     if(token){
         document.cookie=`token=${token}; path=/; max-age=${7 * 24 * 60 * 60}; SameSite=None; Secure`
