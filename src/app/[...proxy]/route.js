5249e5d9b0ca1ff01e81436935d219fbd5174102
Sabarish575 <sabarish2k3@gmail.com>
2026-03-07 10:56:51 +0530

    removed noises

--- src/app/[...proxy]/route.js
+++ src/app/[...proxy]/route.js
@@ -1,51 +1,39 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-async function handler(request, {params}){
+async function handler(request, { params }) {
 
+  const cookieStore = cookies();
+  const jwt = cookieStore.get("jwt")?.value;
 
-    //read the cookies
-    const cookieStore=cookies();
+  if (!jwt) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
-    //extracting the jwt token
-    const jwt=cookieStore.get("jwt")?.value;
+  const proxyPath = params.proxy.join("/");
+  const { searchParams } = new URL(request.url);
+  const queryString = searchParams.toString();
 
-
-    if(!jwt){
-        return NextResponse.json({
-            error:"Unauthorized"
-        },
-        {status:401})
-    }
-
-    //since in next js folder structure act likes like a url
-    //[...proxy] defines path like /user/userfeed but it will looks like an array we are inserting the "/" on split
-    const proxyPath=params.proxy.join("/")
-    const { searchParams } = new URL(request.url);
-    const queryString = searchParams.toString();
-  
-    console.log(queryString);
-
-    const targetUrl = `https://quietconnect-backend.onrender.com/${proxyPath}${
+  const targetUrl = `https://quietconnect-backend.onrender.com/${proxyPath}${
     queryString ? "?" + queryString : ""
   }`;
 
-try {
+  try {
     const response = await fetch(targetUrl, {
-        method: request.method,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`,
-        },
-        ...(request.method !== "GET" && { body: await request.text() }),
+      method: request.method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      ...(request.method !== "GET" && { body: await request.text() }),
     });
 
     const data = await response.json();
     return NextResponse.json(data, { status: response.status });
-} catch (err) {
+  } catch (err) {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 }
-
 export const GET = handler;
 export const POST = handler;
 export const PUT = handler;
