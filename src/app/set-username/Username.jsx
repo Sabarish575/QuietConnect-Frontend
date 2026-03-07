bbb5f06ae135a519c91bcd6db669b00b7897c067
Sabarish575 <sabarish2k3@gmail.com>
2026-03-07 11:10:25 +0530

    removed noises

--- src/app/set-username/Username.jsx
+++ src/app/set-username/Username.jsx
@@ -34,11 +34,6 @@ export default function Username() {
         {
           username,
           bio,
-        },
-        {
-                                headers:{
-            Authorization: `Bearer ${token}`
-          },
         }
       );
 
