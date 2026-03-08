e617d7990663bdf91d4be3ef4cbb7090dea85e37
Sabarish575 <sabarish2k3@gmail.com>
2026-03-08 07:23:50 +0530

    added async

--- src/app/api/auth/token/route.js
+++ src/app/api/auth/token/route.js
@@ -1,9 +1,10 @@
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(){
 
-    const cookieStore=cookies();
-    const jwt=cookieStore.get("jwt").value;
+    const cookieStore=await cookies();
+    const jwt=cookieStore.get("jwt")?.value;
 
     if(!jwt){
         return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
